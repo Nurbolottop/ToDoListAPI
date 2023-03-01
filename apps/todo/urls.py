f5b2a6045ec9419673b5720719392d10bf25ca4b
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-03-01 10:58:53 -0500

    delete all api

--- apps/todo/urls.py
+++ apps/todo/urls.py
@@ -1,12 +1,13 @@
-from django.contrib import admin
+from django.urls import path
 from rest_framework.routers  import DefaultRouter
 
-from .views import ToDoAPI
+from .views import ToDoAPI,ToDoAllDestroyAPIView
+
 router = DefaultRouter()
 router.register("todo", ToDoAPI, basename="users")
 
 
 urlpatterns = [
-
+    path('api/todo/destroy/all/', ToDoAllDestroyAPIView.as_view(), name = "todo_all_destroy")
 ]
 urlpatterns+=router.urls
--- apps/todo/views.py
+++ apps/todo/views.py
@@ -1,5 +1,7 @@
 from rest_framework import mixins
+from rest_framework import generics
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.response import Response
 from rest_framework import filters
 from .serializers import ToDo,ToDoSerializer
 
@@ -14,4 +16,11 @@ class ToDoAPI(GenericViewSet,
     serializer_class = ToDoSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ('title', 'description')
-    
+
+class ToDoAllDestroyAPIView(generics.DestroyAPIView):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+    def delete(self, request, *args, **kwargs):
+        todo = ToDo.objects.filter()
+        todo = [t for t in todo.delete()]
+        return Response({'delete' : 'Все такски удалены'})
